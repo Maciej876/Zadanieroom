74f979cc78b4c554157a2d892f1ffbebd005d126
Maciej876 <maciek.falek4@gmail.com>
2026-03-06 10:50:58 +0100

    Encj Bazy danych

---
+++ app/src/main/java/com/example/roomzadanie/Ksiazki.java
@@ -0,0 +1,66 @@
+package com.example.roomzadanie;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "Ksiazki_tabela")
+public class Ksiazki {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String nazwa;
+    private String gatunek;
+    @ColumnInfo(name = "autor")
+    private String autor;
+    @ColumnInfo(name = "ilosc_stron,")
+    private int iloscStron;
+
+    public Ksiazki(String nazwa, String skladniki, String autor, int iloscStron) {
+        id = 0;
+        this.nazwa = nazwa;
+        this.gatunek = skladniki;
+        this.autor = autor;
+        this.iloscStron = iloscStron;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getGatunek() {
+        return gatunek;
+    }
+
+    public void setGatunek(String gatunek) {
+        this.gatunek = gatunek;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getIloscStron() {
+        return iloscStron;
+    }
+
+    public void setIloscStron(int iloscStron) {
+        this.iloscStron = iloscStron;
+    }
+}
+
