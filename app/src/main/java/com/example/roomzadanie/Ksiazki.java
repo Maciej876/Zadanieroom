8c0046656d361314aa722edabd23dd5160c80a51
Maciej876 <maciek.falek4@gmail.com>
2026-03-19 23:29:22 +0100

    Update Ksiazki.java

--- app/src/main/java/com/example/roomzadanie/Ksiazki.java
+++ app/src/main/java/com/example/roomzadanie/Ksiazki.java
@@ -4,62 +4,40 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity (tableName = "Ksiazki_tabela")
+@Entity(tableName = "ksiazki_tabela")
 public class Ksiazki {
+
     @PrimaryKey(autoGenerate = true)
     private int id;
+
     private String nazwa;
     private String gatunek;
+
     @ColumnInfo(name = "autor")
     private String autor;
-    @ColumnInfo(name = "ilosc_stron,")
+
+    @ColumnInfo(name = "ilosc_stron")
     private int iloscStron;
 
     public Ksiazki(String nazwa, String gatunek, String autor, int iloscStron) {
-        id = 0;
         this.nazwa = nazwa;
         this.gatunek = gatunek;
         this.autor = autor;
         this.iloscStron = iloscStron;
     }
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public String getNazwa() {
-        return nazwa;
-    }
+    public String getNazwa() { return nazwa; }
+    public void setNazwa(String nazwa) { this.nazwa = nazwa; }
 
-    public void setNazwa(String nazwa) {
-        this.nazwa = nazwa;
-    }
+    public String getGatunek() { return gatunek; }
+    public void setGatunek(String gatunek) { this.gatunek = gatunek; }
 
-    public String getGatunek() {
-        return gatunek;
-    }
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
 
-    public void setGatunek(String gatunek) {
-        this.gatunek = gatunek;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getIloscStron() {
-        return iloscStron;
-    }
-
-    public void setIloscStron(int iloscStron) {
-        this.iloscStron = iloscStron;
-    }
+    public int getIloscStron() { return iloscStron; }
+    public void setIloscStron(int iloscStron) { this.iloscStron = iloscStron; }
 }
-
